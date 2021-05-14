3b74b32de22e4aa34fd0b80b2bfb4131ed795f89
罗卜大 <7628035+wx_8387aaaa57@user.noreply.gitee.com>
2021-05-14 20:05:51 +0800

    接口和类的关系

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import './enum'
 import './interface'
 import './function'
 import './class'
+import './class-interface'
 let hello: string = 'hello TypeScript'
 document.querySelectorAll('.app')[0].innerHTML = hello
