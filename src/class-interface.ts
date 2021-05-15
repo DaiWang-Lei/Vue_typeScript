c2a1c6c9293be2b5ce4d340be5c7ebe237dc00fe
罗卜大 <7628035+wx_8387aaaa57@user.noreply.gitee.com>
2021-05-15 08:34:01 +0800

    交叉类型和联合类型

--- src/class-interface.ts
+++ src/class-interface.ts
@@ -116,3 +116,72 @@ function getLanguage(type: Type, x: string | number) {
     }
     return lang
 }
+
+
+
+
+// 交叉类型与联合类型
+// 交叉类型
+interface DogInterface {
+    run(): void
+}
+
+interface CatInterface {
+    jump(): void
+}
+
+let pet: DogInterface & CatInterface = {
+    run() { },
+    jump() { }
+}
+
+// 联合类型
+let aLianhe: number | string = '1'
+let bLianhe: '1' | 'b'
+let cLianhe: 1 | 2 | 3
+
+// 对象的联合类型
+class DogLian implements DogInterface {
+    run() { }
+    eat() { }
+}
+
+class CatLian implements CatInterface {
+    jump() { }
+    eat() { }
+}
+
+enum Master { Boy, Girl }
+function getPet(master: Master) {
+    let pet = master === Master.Boy ? new DogLian() : new CatLian()
+    pet.eat()
+    return pet
+}
+
+interface Square {
+    kind: 'square',
+    size: number
+}
+interface Rectangle {
+    kind: 'rectangle',
+    width: number,
+    height: number,
+}
+interface Circle {
+    kind: 'circle',
+    r: number
+}
+type Shape = Square | Rectangle | Circle
+function area(s: Shape) {
+    switch (s.kind) {
+        case 'square':
+            return s.size * s.size
+        case 'rectangle':
+            return s.width * s.height
+        case 'circle':
+            return Math.PI * s.r ** 2
+        default:
+            return ((e:never)=>{throw new Error(e)})(s)
+    }
+}
+console.log(area({ kind: 'circle', r: 1 }))
